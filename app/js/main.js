31c6317742180e3c467769945cd0dde05e71e07d
asubota <asubota@cogniance.com>
2014-04-01 12:32:31 +0300

    geocoords from photo exif gps data

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,19 @@
 $(function() {
 
-	var map = L.map('map').setView([50.414124,30.522423], 13);
+  var map = L.map('map').setView([50.414124,30.522423], 13);
 
-	L.tileLayer('http://{s}.tile.cloudmade.com/8ee2a50541944fb9bcedded5165f09d9/997/256/{z}/{x}/{y}.png', {
-	maxZoom: 18
-	}).addTo(map);
+  L.Icon.Default.imagePath = '/images/';
+  L.tileLayer('http://{s}.tile.cloudmade.com/8ee2a50541944fb9bcedded5165f09d9/997/256/{z}/{x}/{y}.png', {
+    maxZoom: 18
+  }).addTo(map);
 
-	$('#content .menu .item').tab();
+  $('#content .menu .item').tab();
+
+  $.getJSON('/getCoords', function(data) {
+    L.marker([data.lat, data.lng])
+      .addTo(map)
+      .bindPopup('Atata :)')
+      .openPopup();
+  });
 
 });
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,65 @@
-
-/*
- * GET home page.
- */
+var ExifImage = require('exif').ExifImage,
+  when = require('when');
 
 exports.index = function(req, res){
   res.send('Please, navigate to <a href="/index.html">index.html</a> page');
 };
+
+exports.getCoords = function(req, res) {
+  var imgFile = 'test.jpg';
+
+  __getCoords(imgFile).then(
+    function(data) {
+      res.send(data);
+    },
+    function(error) {
+        console.log(error);
+    }
+  );
+
+};
+
+/* private functions */
+function __getCoords(imgFile) {
+    var deferred = when.defer();
+
+    try {
+        new ExifImage({image : imgFile}, function (error, exifData) {
+            if (error) {
+                deferred.reject(new Error(error.message));
+            }
+            else {
+                deferred.resolve(calculate(exifData.gps));
+            }
+        });
+    } catch (error) {
+        deferred.reject(new Error(error.message));
+    }
+
+    function calculate(data) {
+        var GPSLat = data.GPSLatitude,
+            GPSLng = data.GPSLongitude,
+            GPSLatRef = data.GPSLatitudeRef,
+            GPSLngRef = data.GPSLongitudeRef,
+            result = {};
+
+        function parse(data) {
+            return data[0] + data[1]/60 + data[2]/3600;
+        }
+
+        result.lat = parse(GPSLat);
+        result.lng = parse(GPSLng);
+
+        if (GPSLatRef.toLowerCase() === 's') {
+            result.lat = -1 * result.lat;
+        }
+
+        if (GPSLngRef.toLowerCase() === 'w') {
+            result.lng = -1 * result.lng;
+        }
+
+        return result;
+    }
+
+    return deferred.promise;
+}
